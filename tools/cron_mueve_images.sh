afa097c665f9efd9ae6b1171e59930cb3d091b0f
pbullian <pbullian@unsam.edu.ar>
2014-05-05 23:36:57 -0300

    errors

--- tools/cron_mueve_images.sh
+++ tools/cron_mueve_images.sh
@@ -3,10 +3,9 @@
 #
 #copia las imagenes logicas al storage
 #
-#setear en crontab: 0 2 30 4 3 sh /root/PB/cron_mueve_imagen.sh bk-tobatest-120601 VG2 f 2> /root/PB/bk-tobatest-120601.log
 
 #
-STORAGE=10.1.71.107
+STORAGE=192.168.1.1
 PATH_STORAGE=/storage/images_a_mano/
 PATH_XEN_VM=/etc/xen/*
 
@@ -25,11 +24,11 @@ else
 			exit 1
 		else
 	                printf "\nCopiando..."
-			dd if=/dev/$2/$1 | gzip -c --fast | ssh root@10.1.71.107 'dd of=/storage/images_a_mano/'$1'.img.gz'
+			dd if=/dev/$2/$1 | gzip -c --fast | ssh root@$STORAGE 'dd of='$PATH_STORAGE''$1'.img.gz'
 		fi
 	else
 		printf "\nForzado activado:\nCopiando..."
-                dd if=/dev/$2/$1 | gzip -c --fast | ssh  root@10.1.71.107 'dd of='$PATH_STORAGE''$1'.img.gz'  
+                dd if=/dev/$2/$1 | gzip -c --fast | ssh  root@$STORAGE 'dd of='$PATH_STORAGE''$1'.img.gz'  
 
 	fi
 fi
