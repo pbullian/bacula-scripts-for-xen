9c852336c37bf60a2a4d49f71c4603f1993df0cb
pbullian <pbullian@unsam.edu.ar>
2014-05-05 23:41:08 -0300

    update del server1

--- postrun_image.sh
+++ postrun_image.sh
@@ -1,31 +1,36 @@
-#!/bin/bash
+#!/bin/bash
 
 PRENDIO="false";
-IP=$2
-NOMBRE=$1
+IP=$1
+NOMBRE=$2
 VOL=$3
 IMAGEN=$4
+ARCHIVO_CREACION=$5
 count=0
+CARPETA_BACKS=/backups
+XENPWD=/etc/xen/apagadas
 
 #borro la imagen creada
-rm -rf /backs_tmp/'$IMAGEN'.img.gz
+rm -rf $CARPETA_BACKS/$IMAGEN.img.gz
 
-xm create /etc/xen/auto/$NOMBRE
+xm create $XENPWD/$ARCHIVO_CREACION
 
 #espero hasta que aparesca o hasta que pasen 2 minutos
-while [ "$PRENDIO" != "false" -o $count = 90 ] 
+while [ "$PRENDIO" != "true" -o $count = 90 ] 
 do
   if [[ -n $(xm list|grep $NOMBRE) ]]; then
-        APAGO="false";
-	count=$(($count + 1));
+        PRENDIO="true";
   else
-	APAGO="true";
+	PRENDIO="false";
+	count=$(($count + 1));
   fi
 sleep 2;
 done
 
-if [ $count = 90 ];then
+if [ "$PRENDIO" != "true" ];then
+#printf "\n\nerror\n\n"
   exit 1
 else
+#printf "\n\nok\n\n"
   exit 0
 fi
