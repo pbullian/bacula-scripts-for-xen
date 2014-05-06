4af895b555b9b0c70eeab5ecdfcac3df8d8a5207
pbullian <pbullian@unsam.edu.ar>
2014-05-05 23:42:01 -0300

    update del server1

--- prerun_image.sh
+++ prerun_image.sh
@@ -7,12 +7,12 @@
 APAGO="false";
 
 #esto se deberia de tomar de un archivo de propiedades
-NOMBRE=$1
-IP=$2
+NOMBRE=$2
+IP=$1
 VOL=$3
 IMAGEN=$4
 USER=$5
-CARPETA_BACKS=/backs
+CARPETA_BACKS=/backups
 count=0
 #
 #FUN BEGINSSS
@@ -21,21 +21,27 @@ count=0
 ssh $USER@$IP 'halt'
 
 #espero hasta que desaparezca o 3 min
-while [ "$APAGO" != "false" -o $count = 90 ] 
+while [ "$APAGO" != "true" -o $count = 90 ] 
 do
   if [[ -n $(xm list|grep $NOMBRE) ]]; then
-	APAGO="true";
+	APAGO="false";
 	count=$(($count + 1))
+  else
+	APAGO="true";
   fi
 sleep 2;
 done
 
+if [ "$APAGO" != "true" ];then
+  exit 1;
+fi
+
 #una vez que apago, comprimo la imagen
-dd if=/dev/$VOL/$IMAGEN | gzip -c --fast | dd of='$CARPETA_BACKS'/'$IMAGEN'.img.gz
+dd if=/dev/$VOL/$IMAGEN | gzip -c --fast | dd of=$CARPETA_BACKS/$IMAGEN.img.gz
 
 #si todo fallo, me tiro a un pozo y devuelvo 1
 if [[ -n $(ls $CARPETA_BACKS |grep $IMAGEN) ]]; then
-	exit 1;
-else
 	exit 0;
+else
+	exit 1;
 fi
